b0f0bbc574b87d3e25a1eaa24a174bf77ede4a0f
Damngz <da.munozgz@gmail.com>
2024-06-24 00:08:47 -0400

    fix: bug categories

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [NavbarComponent, HeaderComponent, FooterComponent],
+  imports: [RouterLink, NavbarComponent, HeaderComponent, FooterComponent],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css'
 })
